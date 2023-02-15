5dd905f79e77df8cdea71d053a3474dfd13ef365
FuadNabi <fuad.nebiyev95@mail.ru>
2023-02-15 13:46:38 +0400

    Fix lintern error

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,12 +1,12 @@
 require './book'
 require './person'
 
-person = Person.new(20, 'Fuad')
+person = Person.new(20, 'Bash')
 
 describe Person do
   context 'Testing the Person Class' do
     it 'The correct_name method should return person name' do
-      expect(person.correct_name).to eq 'Fuad'
+      expect(person.correct_name).to eq 'Bash'
     end
 
     it 'the can_use_services? method should return true' do
